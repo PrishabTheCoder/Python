1a41b1ac4fd653892b74757354b2f939e33d8b7e
PrishabTheCoder <prishabshrestha123@gmail.com>
2026-08-26 19:43:33 -0400

    initial commit

---
+++ Module 1 lesson 1 Setup/Legacy activity..py
@@ -0,0 +1,6 @@
+print("welcome to the world of programing")
+print("4")
+print("hello python \n")
+print("python keywords are...")
+import keyword
+print(keyword.kwlist)
--- Module 1 lesson 1 Setup/printstatement.py
+++ Module 1 lesson 1 Setup/printstatement.py
@@ -1,1 +1,4 @@
-print("first program")
+print("Hello world")
+print("My name is Prishab")
+print("I like to play games")
+print("the answer to 4 + 2 is",4 + 2)
